a67d25858bd5db0e6c2ff4959cc719aea025c7f2
palestraitalia <141340393+palestraitalia@users.noreply.github.com>
2025-04-04 08:35:38 -0300

    .

--- script2.js
+++ script2.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
-
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
-  
 botaoDeAcessibilidade.addEventListener('click', function () {
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
 })
+
     let tamanhoAtualFonte = 1;
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
@@ -14,7 +13,7 @@ botaoDeAcessibilidade.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
-
+    
     diminuiFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
